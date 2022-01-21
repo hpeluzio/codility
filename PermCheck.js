a07305ec21b9cbb3e7e2c5ecd06e1bbd6d06b6dd
Henrique Peluzio <hpeluzio@gmail.com>
2022-01-21 11:41:00 -0300

    Update PermCheck.js

--- PermCheck.js
+++ PermCheck.js
@@ -1,17 +1,7 @@
 function solution(A) {
-  let arrSet = new Set(A);
-
-  if (arrSet.size !== A.length) return 0;
-
-  A.sort((a, b) => a - b);
-
-  for (let i = 1; i < A.length; i++) {
-    if (A[i] - A[i - 1] !== 1) {
-      return 0;
-    }
-  }
-
-  return 1;
+  const arrSet = new Set(A);
+  const max = Math.max(...A);
+  return arrSet.size === max && arrSet.size === A.length ? 1 : 0;
 }
 
-console.log("solution: ", solution([4, 1, 3, 7]));
+console.log("solution: ", solution([4, 1, 3, 2]));
