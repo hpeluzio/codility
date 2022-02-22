d5fa67cc5600c65bcd3f7e56b460d946469074e8
Henrique Peluzio <hpeluzio@gmail.com>
2022-02-22 16:15:47 -0300

    Update GenomicRangeQuery.js

--- Youtube/GenomicRangeQuery.js
+++ Youtube/GenomicRangeQuery.js
@@ -1,5 +1,27 @@
 function solution(S, P, Q) {
-  return [2, 4, 1];
+  //CAGCCTA
+  //A=1, C=2, G=3, T=4
+  let minimals = [];
+
+  for (let i = 0; i < P.length; i++) {
+    let subString = S.substring(P[i], Q[i] + 1);
+
+    if (subString.indexOf('A') !== -1) {
+      minimals[i] = 1;
+      continue;
+    }
+    if (subString.indexOf('C') !== -1) {
+      minimals[i] = 2;
+      continue;
+    }
+    if (subString.indexOf('G') !== -1) {
+      minimals[i] = 3;
+      continue;
+    }
+    minimals[i] = 4;
+  }
+
+  return minimals;
 }
 
 console.log(solution('CAGCCTA', [2, 5, 0], [4, 5, 6]));
